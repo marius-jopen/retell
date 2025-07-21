a1e5fb88ca3ec11916ca3b7d2d0eede60bf6b8c9
Marius Jopen <mail@mariusjopen.com>
2025-07-21 12:00:24 -0400

    🔧 FIX: Remove unused imports and variables
    - Remove unused useRouter import from author edit page
    - Remove unused episode variable from new episode page
    - Fix any type in author upload page
    - Clean up unused response variables
    
    Continues ESLint error cleanup for Vercel build

--- src/app/author/podcasts/[id]/edit/page.tsx
+++ src/app/author/podcasts/[id]/edit/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { useToast } from '@/components/ui/toast'
